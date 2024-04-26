18a4ab1c55e7beee9b6da0add465ca7f605dc917
August-IE <Aeromosele9@gmail.com>
2024-04-26 00:32:35 +0000

    Average score

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -2,22 +2,22 @@
 -- computes and stores the average score for a student.
 DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 DELIMITER $$
-
 CREATE PROCEDURE ComputeAverageScoreForUser (user_id INT)
 BEGIN
-    DECLARE total_score FLOAT;
-    DECLARE projects_count INT;
+    DECLARE total_score INT DEFAULT 0;
+    DECLARE projects_count INT DEFAULT 0;
 
-    -- Compute total score and number of projects
-    SELECT SUM(score), COUNT(*)
-    INTO total_score, projects_count
-    FROM corrections
-    WHERE user_id = user_id;
+    SELECT SUM(score)
+        INTO total_score
+        FROM corrections
+        WHERE corrections.user_id = user_id;
+    SELECT COUNT(*)
+        INTO projects_count
+        FROM corrections
+        WHERE corrections.user_id = user_id;
 
-    -- Update average score for the user
     UPDATE users
-    SET average_score = IFNULL(total_score / NULLIF(projects_count, 0), 0)
-    WHERE id = user_id;
+        SET users.average_score = IF(projects_count = 0, 0, total_score / projects_count)
+        WHERE users.id = user_id;
 END $$
-
 DELIMITER ;
